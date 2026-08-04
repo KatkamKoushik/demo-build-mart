8878cb09dd355cd80c76f788af8b783c49b18fd3
koushik katkam <koushikkatkam@gmail.com>
2026-08-04 21:40:16 +0530

    fix: Typo in Header closing tag

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -124,7 +124,7 @@ export default function Header() {
           <MapPin className="w-3 h-3 text-cobalt" />
           {pinCode}
         </button>
-      </motion.header>
+      </header>
 
       <LocationModal
         isOpen={isModalOpen}
